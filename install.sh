5483dc6777edd40ebdae3632d8bd787377a761d8
Mauro Bernardini <mauro.bernardini@uzh.ch>
2026-08-11 18:08:43 +0200

    removed unwanted file tracking

--- install.sh
+++
@@ -1,7 +0,0 @@
-# convenience script for full installation, linting, testing and docs building
-pip install -e .
-black smudgy/
-ruff check smudgy/ --fix
-pytest -rs
-rm -rf docs/_build
-python3 -m sphinx -b html docs docs/_build/html -j 8
