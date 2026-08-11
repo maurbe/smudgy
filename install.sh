69fa536aeebeb80d836aa7bd424095f182bece59
Mauro Bernardini <mauro.bernardini@uzh.ch>
2026-08-11 18:04:22 +0200

    updated gitignore

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-#pip install .
-#black smudgy/
-#ruff check smudgy/ --fix
-#pytest -rs
+# convenience script for full installation, linting, testing and docs building
+pip install -e .
+black smudgy/
+ruff check smudgy/ --fix
+pytest -rs
 rm -rf docs/_build
 python3 -m sphinx -b html docs docs/_build/html -j 8
